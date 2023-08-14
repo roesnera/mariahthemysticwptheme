5349e057d87de031e7475c3d81c2c36b5f69ab20
roesnera <adam.roesner@generation.org>
2023-08-14 13:37:31 -0400

    initial commit for mariah's ecommerce site

--- functions.php
+++ functions.php
@@ -1,8 +1,9 @@
 <?php
+if(!function_exists('mariahs_styles')) : 
+    function mariahs_styles() {
+        wp_enqueue_style('mariahs-style', get_stylesheet_uri(), array(), wp_get_theme()->get('version'));
+    }
+endif;
 
-function presleys_styles() {
-    wp_enqueue_style('presleys-style', get_stylesheet_uri(), array(), wp_get_theme()->get('version'));
-}
-
-add_action('wp_enqueue_scripts', 'presleys_styles');
+add_action('wp_enqueue_scripts', 'mariahs_styles');
 ?>
