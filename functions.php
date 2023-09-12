37af321aa5add39f12ab094b0bceded5480210d3
roesnera <adam.roesner@generation.org>
2023-09-12 16:41:21 -0400

    begins to port over react page to wp

--- functions.php
+++ functions.php
@@ -2,6 +2,12 @@
 if(!function_exists('mariahs_styles')) : 
     function mariahs_styles() {
         wp_enqueue_style('mariahs-style', get_stylesheet_uri(), array(), wp_get_theme()->get('version'));
+    
+        if(is_page('collab')) {
+            wp_enqueue_style('collab', get_template_directory_uri() . '/styles/collab.css');
+            wp_enqueue_style('iphone', get_template_directory_uri() . '/styles/iphone.css');
+        }
+    
     }
 endif;
 
---
+++ page-collab.php
@@ -0,0 +1,231 @@
+<?php get_header(); ?>
+
+<main>
+    <section class="section">
+        <h2 class="section-title">video</h2>
+        <div class="flex-container video-grid">
+            <div class="phone-item"><a href="https://www.tiktok.com/@mariahthemystic/video/7224959282142367022?_r=1&amp;_t=8box3zOQyKI" target="_blank" rel="noreferrer">
+                    <div class="marvel-device iphone-x">
+                        <div class="notch">
+                            <div class="camera"></div>
+                            <div class="speaker"></div>
+                        </div>
+                        <div class="top-bar"></div>
+                        <div class="sleep"></div>
+                        <div class="bottom-bar"></div>
+                        <div class="volume"></div>
+                        <div class="overflow">
+                            <div class="shadow shadow--tr"></div>
+                            <div class="shadow shadow--tl"></div>
+                            <div class="shadow shadow--br"></div>
+                            <div class="shadow shadow--bl"></div>
+                        </div>
+                        <div class="inner-shadow"></div>
+                        <div class="phone-screen screen__1"></div>
+                    </div>
+                </a></div>
+            <div class="phone-item"><a href="https://www.tiktok.com/@mariahthemystic/video/7220554105456971050?_d=secCgYIASAHKAESPgo8vvqFPCXt%2FTR228JgAF5rbT04pFlOzYy7oNCRbviQBlYnyhz4z12i9Rmt6Q1dE3xWgu%2F10NdXaUINO9AtGgA%3D&amp;_r=1&amp;checksum=40215ff17d73ec18a9383f9bd64677cbb419f267be9805c6fcf95f7946b7be8a&amp;preview_pb=0&amp;sec_user_id=MS4wLjABAAAAS7RL9zALxjO0bHO8YSlbffp6LqC-RPn3AlKehPKDfx3qyVHfdjSG3KI_CEsm4hm7&amp;share_app_id=1233&amp;share_item_id=7220554105456971050&amp;share_link_id=FBA1D625-87E9-4327-A88C-B6B1979B1314&amp;sharer_language=en&amp;source=h5_m&amp;timestamp=1682525603&amp;tt_from=copy&amp;u_code=dhd92598c3929k&amp;ug_btm=b8727%2Cb2878&amp;user_id=6937784711738377221&amp;utm_campaign=client_share&amp;utm_medium=ios&amp;utm_source=copy" target="_blank" rel="noreferrer">
+                    <div class="marvel-device iphone-x">
+                        <div class="notch">
+                            <div class="camera"></div>
+                            <div class="speaker"></div>
+                        </div>
+                        <div class="top-bar"></div>
+                        <div class="sleep"></div>
+                        <div class="bottom-bar"></div>
+                        <div class="volume"></div>
+                        <div class="overflow">
+                            <div class="shadow shadow--tr"></div>
+                            <div class="shadow shadow--tl"></div>
+                            <div class="shadow shadow--br"></div>
+                            <div class="shadow shadow--bl"></div>
+                        </div>
+                        <div class="inner-shadow"></div>
+                        <div class="phone-screen screen__2"></div>
+                    </div>
+                </a></div>
+            <div class="phone-item"><a href="https://www.tiktok.com/@mariahthemystic/video/7216410379809934638?_r=1&amp;_t=8box9GnGhLO" target="_blank" rel="noreferrer">
+                    <div class="marvel-device iphone-x">
+                        <div class="notch">
+                            <div class="camera"></div>
+                            <div class="speaker"></div>
+                        </div>
+                        <div class="top-bar"></div>
+                        <div class="sleep"></div>
+                        <div class="bottom-bar"></div>
+                        <div class="volume"></div>
+                        <div class="overflow">
+                            <div class="shadow shadow--tr"></div>
+                            <div class="shadow shadow--tl"></div>
+                            <div class="shadow shadow--br"></div>
+                            <div class="shadow shadow--bl"></div>
+                        </div>
+                        <div class="inner-shadow"></div>
+                        <div class="phone-screen screen__3"></div>
+                    </div>
+                </a></div>
+            <div class="phone-item"><a href="https://www.tiktok.com/@mariahthemystic/video/7223837953926319406?_r=1&amp;_t=8box5TawS4p" target="_blank" rel="noreferrer">
+                    <div class="marvel-device iphone-x">
+                        <div class="notch">
+                            <div class="camera"></div>
+                            <div class="speaker"></div>
+                        </div>
+                        <div class="top-bar"></div>
+                        <div class="sleep"></div>
+                        <div class="bottom-bar"></div>
+                        <div class="volume"></div>
+                        <div class="overflow">
+                            <div class="shadow shadow--tr"></div>
+                            <div class="shadow shadow--tl"></div>
+                            <div class="shadow shadow--br"></div>
+                            <div class="shadow shadow--bl"></div>
+                        </div>
+                        <div class="inner-shadow"></div>
+                        <div class="phone-screen screen__4"></div>
+                    </div>
+                </a></div>
+            <div class="phone-item"><a href="https://www.tiktok.com/@mariahthemystic/video/7201582081565084970?_d=secCgYIASAHKAESPgo8bRLM%2Bnp%2FDG8jriDBNjK%2FCMzBwvNLiJV6wLpHPiJG4iyuUzifLHoe2OuJlZAvYiEO0neMgUdb1X%2FCL5CkGgA%3D&amp;_r=1&amp;checksum=d7393e92ff4377c1b496b8bd47fce6888c38e88a04e16d6e6db314eb659c1ad0&amp;preview_pb=0&amp;sec_user_id=MS4wLjABAAAAS7RL9zALxjO0bHO8YSlbffp6LqC-RPn3AlKehPKDfx3qyVHfdjSG3KI_CEsm4hm7&amp;share_app_id=1233&amp;share_item_id=7201582081565084970&amp;share_link_id=86D01F3A-A53B-4E29-ADF3-1D64D121D044&amp;sharer_language=en&amp;source=h5_m&amp;timestamp=1682525955&amp;tt_from=copy&amp;u_code=dhd92598c3929k&amp;ug_btm=b8727%2Cb2878&amp;user_id=6937784711738377221&amp;utm_campaign=client_share&amp;utm_medium=ios&amp;utm_source=copy" target="_blank" rel="noreferrer">
+                    <div class="marvel-device iphone-x">
+                        <div class="notch">
+                            <div class="camera"></div>
+                            <div class="speaker"></div>
+                        </div>
+                        <div class="top-bar"></div>
+                        <div class="sleep"></div>
+                        <div class="bottom-bar"></div>
+                        <div class="volume"></div>
+                        <div class="overflow">
+                            <div class="shadow shadow--tr"></div>
+                            <div class="shadow shadow--tl"></div>
+                            <div class="shadow shadow--br"></div>
+                            <div class="shadow shadow--bl"></div>
+                        </div>
+                        <div class="inner-shadow"></div>
+                        <div class="phone-screen screen__5"></div>
+                    </div>
+                </a></div>
+            <div class="phone-item"><a href="https://www.tiktok.com/@mariahthemystic/video/7216781520663645486?_r=1&amp;_t=8box87gW2ak" target="_blank" rel="noreferrer">
+                    <div class="marvel-device iphone-x">
+                        <div class="notch">
+                            <div class="camera"></div>
+                            <div class="speaker"></div>
+                        </div>
+                        <div class="top-bar"></div>
+                        <div class="sleep"></div>
+                        <div class="bottom-bar"></div>
+                        <div class="volume"></div>
+                        <div class="overflow">
+                            <div class="shadow shadow--tr"></div>
+                            <div class="shadow shadow--tl"></div>
+                            <div class="shadow shadow--br"></div>
+                            <div class="shadow shadow--bl"></div>
+                        </div>
+                        <div class="inner-shadow"></div>
+                        <div class="phone-screen screen__6"></div>
+                    </div>
+                </a></div>
+        </div>
+    </section>
+    <section class="section">
+        <h2 class="section-title">ugc photo</h2>
+        <div class="grid-container picture-group">
+            <figure class="picture picture__1"></figure>
+            <figure class="picture picture__2"></figure>
+            <figure class="picture picture__3"></figure>
+            <figure class="picture picture__4"></figure>
+            <figure class="picture picture__5"></figure>
+            <figure class="picture picture__6"></figure>
+        </div>
+    </section>
+    <section class="section metrics-section">
+        <h2 class="section-title">metrics</h2>
+        <div class="metric-box">
+            <p>5417 tiktok followers</p>
+            <p>128.9k likes</p>
+            <p>1.8k instagram followers</p>
+        </div>
+        <div class="darken">
+            <p class="flex-container darken-flex"><span class="darken-flex-item">13.4% engagement</span><span class="darken-flex-item">96% female</span></p>
+        </div>
+        <h4 class="metric-subtitle">target audience:</h4>
+        <p>young women and queer individuals who are passionate about self expression. this includes fashion, astrology, and makeup. experiencing a sense of magic and childlike wonder in their everyday.</p>
+    </section>
+    <section class="section">
+        <h2 class="section-title">packages and rates</h2>
+        <div class="grid-container packages-container">
+            <div class="package-item">
+                <h4>videos</h4>
+            </div>
+            <div class="package-item">
+                <h4>photos</h4>
+            </div>
+            <div class="package-item packages-right-col">
+                <div class="package-item">
+                    <h4>extra</h4>
+                </div>
+                <div class="package-item package-right-col-small">
+                    <p>usage rights +30%</p>
+                    <p>raw content $50</p>
+                </div>
+            </div>
+            <div class="package-item">
+                <p>1 video - $150</p>
+                <p>3 videos - $300</p>
+                <p>5 videos - $550</p><br>
+                <p>* 1 revision included</p>
+            </div>
+            <div class="package-item">
+                <p>1 photo - $50</p>
+                <p>3 photos - $100</p>
+                <p>10 photos - $200</p>
+            </div>
+            <div class="package-item packages-right-col">
+                <div class="package-item">
+                    <h4>test</h4>
+                </div>
+                <div class="package-item">
+                    <p>4 videos</p>
+                    <p>(4 hooks: 1 body, 1 cta)</p>
+                    <p>*starting at $250</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="section offerings-section">
+        <h2 class="section-title">offerings</h2>
+        <div class="grid-container offerings-grid">
+            <div class="offerings-left">
+                <ul>
+                    <li>voiceover videos</li>
+                    <li>product showcasing and unboxing</li>
+                    <li>testimonials and reviews</li>
+                    <li>inspiration &amp; lifestyle vlog</li>
+                    <li>fashion &amp; beauty vlog</li>
+                    <li>grwm</li>
+                    <li>how to use tutorials </li>
+                    <li>how to style tutorials </li>
+                    <li>creative videos</li>
+                    <li>product &amp; lifestyle pictures</li>
+                    <li>high fashion editorial pictures</li>
+                </ul>
+            </div>
+            <div class="offerings-right">
+                <h4 class="offerings-subtitle">includes:</h4><br>
+                <ul>
+                    <li>15 to 30 seconds</li>
+                    <li>up to 60 seconds</li>
+                    <li>vertical format (9:16)</li>
+                    <li>research on branding &amp; concepts</li>
+                    <li>script &amp; style board inspo</li>
+                    <li>video editing</li>
+                </ul>
+            </div>
+        </div>
+    </section>
+    <section class="section why-section">
+        <h2 class="section-title">why me</h2>
+        <p>my name is Mars (they/them). i have been working in social media and creating content for nearly 5 years now professionally and personally. my interesets include makeup, fashion and astrology. i have a bachelors degree in communication arts and creative writing. i am a self-proclaimed hedonist. i enjoy the simple and magical pleasures of life, i believe in promoting brands that hold similar values and strive to support the world and community in a positive way.</p>
+    </section>
+</main>
+
+<?php get_footer(); ?>
